65ed36da20c1a59de2d021d8c37b9a5da4b057e0
Fagner Brack <github@fagnermartins.com>
2016-03-21 00:06:48 +1100

    Leverage mocha's built-in Promise handling

--- test/unit/routines/bump-json-files.spec.js
+++ test/unit/routines/bump-json-files.spec.js
@@ -8,21 +8,17 @@ const createFileFixtureSync = require(root + "/test/create-file-fixture-sync");
 
 describe("bump-json-files", function() {
 
-  it("should bump the 'minor' version for a system JSON file", function(done) {
+  it("should bump the 'minor' version for a system JSON file", function() {
     const targetFilename = root + "/test/routines/bump-minor.json";
     const removeFileFixtureSync = createFileFixtureSync(targetFilename, JSON.stringify({
       version: "0.0.0"
     }));
-
-    bumpJSONFiles("minor", [targetFilename]).then(function() {
+    return bumpJSONFiles("minor", [targetFilename]).then(function() {
       return loadJSON(targetFilename);
     }).then(function(targetFile) {
       expect(targetFile).to.have.property("version", "0.1.0");
+    }).finally(function() {
       removeFileFixtureSync();
-      done();
-    }).catch(function(err) {
-      removeFileFixtureSync();
-      done(err);
     });
   });
 
