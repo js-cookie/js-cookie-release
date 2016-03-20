fabcd4f8c9f2a2d6b0b8bf345cb334a75b788c93
Fagner Brack <github@fagnermartins.com>
2016-03-20 23:13:06 +1100

    Create json fixture on-the-fly

--- test/routines/bump-json-files.spec.js
+++ test/routines/bump-json-files.spec.js
@@ -5,16 +5,34 @@ const loadJSON = Promise.promisify(require("json-update").load);
 
 const bumpJSONFiles = require(root + "/src/routines/bump-json-files");
 
+const write = require("write");
+const del = require("delete");
+const createFileFixtureSync = function(fileName, contents) {
+  write.sync(fileName, contents);
+  return function removeFileFixtureSync() {
+    del.sync(fileName);
+  };
+};
+
 describe("bump-json-files", function() {
+
   it("should bump the 'minor' version for a system JSON file", function(done) {
-    bumpJSONFiles("minor", [root + "/test/routines/bump-json-files/bump-minor.json"])
-    .then(function() {
-      return loadJSON(root + "/test/routines/bump-json-files/bump-minor.json");
-    })
-    .then(function(targetFile) {
+    const targetFilename = root + "/test/routines/bump-minor.json";
+    const removeFileFixtureSync = createFileFixtureSync(targetFilename, JSON.stringify({
+      version: "0.0.0"
+    }));
+
+    bumpJSONFiles("minor", [targetFilename]).then(function() {
+      return loadJSON(targetFilename);
+    }).then(function(targetFile) {
       expect(targetFile).to.have.property("version", "0.1.0");
+      removeFileFixtureSync();
       done();
     })
-    .catch(done);
+    .catch(function(err) {
+      removeFileFixtureSync();
+      done(err);
+    });
   });
+
 });
