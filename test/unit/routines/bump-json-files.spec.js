16b33f4501ba2bf8615af349bba681c66e44c238
Fagner Brack <github@fagnermartins.com>
2016-03-21 00:24:16 +1100

    Use root to create the fixture file

--- test/unit/routines/bump-json-files.spec.js
+++ test/unit/routines/bump-json-files.spec.js
@@ -9,7 +9,7 @@ const createFileFixtureSync = require(root + "/test/create-file-fixture-sync");
 describe("bump-json-files", function() {
 
   it("should bump the 'minor' version for a system JSON file", function() {
-    const targetFilename = root + "/test/routines/bump-minor.json";
+    const targetFilename = "bump-minor.json";
     const removeFileFixtureSync = createFileFixtureSync(targetFilename, JSON.stringify({
       version: "0.0.0"
     }));
