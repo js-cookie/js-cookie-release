4bd6993846f579a9494c3ee39f23ba494e7de4c3
Fagner Brack <github@fagnermartins.com>
2016-10-16 00:41:48 +1100

    Rename start-git-repo to start-git-repo-with-server
    This way it is not confused with the create-git-repo-fixture

--- test/unit/routines/git-push.spec.js
+++ test/unit/routines/git-push.spec.js
@@ -1,13 +1,13 @@
 const root = require("app-root-path");
 
-const startGitRepo = require(root + "/test/fixture/start-git-repo");
+const startGitRepoWithServer = require(root + "/test/fixture/start-git-repo-with-server");
 const gitPush = require(root + "/src/routines/git-push");
 const gitCommit = require(root + "/src/routines/git-commit");
 
 describe("git-push", function() {
   it("should push the repository sucessfully to the remote", function() {
     let testRepository;
-    return startGitRepo().then(function(startedTestRepository) {
+    return startGitRepoWithServer().then(function(startedTestRepository) {
       testRepository = startedTestRepository;
       return gitCommit("Dummy commit", testRepository.remotes[0].repository);
     }).then(function() {
