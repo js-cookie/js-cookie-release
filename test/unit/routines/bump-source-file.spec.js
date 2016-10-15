b470a230a2d1609daf5288bed77c72cf1a53351b
Fagner Brack <github@fagnermartins.com>
2016-10-16 02:50:00 +1100

    Implement beforeEach/afterEach for the bump source file specs

--- test/unit/routines/bump-source-file.spec.js
+++ test/unit/routines/bump-source-file.spec.js
@@ -7,26 +7,34 @@ const createFileSync = require(root + "/test/dummy-data/create-file-sync");
 
 describe("bump-source-file", function() {
 
-  it("should bump the 'minor' version using Regular Expression", function() {
+  describe("Given a source file with a version format inside", function() {
     const targetFilename = "bump-minor.js";
-    const removeFileSync = createFileSync(targetFilename, "/* JS Cookie v0.0.0 */");
-    const findVersion = function(fileContent) {
-      const match = /Cookie v([0-9]{1,2}.[0-9]{1,2}.[0-9]{1,2})/.exec(fileContent);
-      const groupMatch = match[1];
-      const startIndex = match.index + "Cookie v".length;
-      const endIndex = startIndex + groupMatch.length;
-      return {
-        startIndex: startIndex,
-        endIndex: endIndex
+    let removeDummyFileSync;
+
+    beforeEach(function() {
+      removeDummyFileSync = createFileSync(targetFilename, "/* JS Cookie v0.0.0 */");
+    });
+
+    afterEach(function() {
+      removeDummyFileSync();
+    });
+
+    it("should edit and bump the 'minor' version using Regular Expression", function() {
+      const findVersion = function(fileContent) {
+        const match = /Cookie v([0-9]{1,2}.[0-9]{1,2}.[0-9]{1,2})/.exec(fileContent);
+        const groupMatch = match[1];
+        const startIndex = match.index + "Cookie v".length;
+        const endIndex = startIndex + groupMatch.length;
+        return {
+          startIndex: startIndex,
+          endIndex: endIndex
+        };
       };
-    };
-    return bumpSourceFile("minor", targetFilename, findVersion).then(function() {
-      return readFile(targetFilename);
-    }).then(function(changedFileContents) {
-      expect(changedFileContents).to.equal("/* JS Cookie v0.1.0 */");
-    }).finally(function() {
-      removeFileSync();
+      return bumpSourceFile("minor", targetFilename, findVersion).then(function() {
+        return readFile(targetFilename);
+      }).then(function(changedFileContents) {
+        expect(changedFileContents).to.equal("/* JS Cookie v0.1.0 */");
+      });
     });
   });
-
 });
