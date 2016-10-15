eafb20a2566f7db17b2108ac5b1941c655271658
Fagner Brack <github@fagnermartins.com>
2016-10-16 02:54:23 +1100

    Implement beforeEach/afterEach for the git commit specs

--- test/unit/routines/git-commit.spec.js
+++ test/unit/routines/git-commit.spec.js
@@ -6,20 +6,28 @@ const gitCommit = require(root + "/src/routines/git-commit");
 
 describe("git-commit", function() {
 
-  it("should create a commit with the given message", function() {
-    let removeGitRepo = () => {};
-    return createGitRepo().then(function(repo) {
-      removeGitRepo = repo.remove;
-      return gitCommit("Release version 0.0.1", repo.repository).then(function(objectId) {
-        expect(objectId).to.be.a("string");
-        return repo.repository.getHeadCommit();
+  describe("Given a dummy git repository running locally", function() {
+    let testRepo;
+
+    beforeEach(function() {
+      return createGitRepo().then(function(createdRepo) {
+        testRepo = createdRepo;
+      });
+    });
+
+    afterEach(function() {
+      testRepo.remove();
+    });
+
+    it("should create a commit with the given message", function() {
+      const commitMessage = "Release version 0.0.1";
+      return gitCommit(commitMessage, testRepo.repository).then(function(commitObjectId) {
+        expect(commitObjectId).to.be.a("string");
+        return testRepo.repository.getHeadCommit();
       }).then(function(headCommit) {
         expect(headCommit).not.to.equal(null);
         expect(headCommit.message()).to.equal("Release version 0.0.1");
       });
-    }).finally(function() {
-      removeGitRepo();
     });
   });
-
 });
