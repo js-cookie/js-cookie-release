75a23c1c1008a2ade05cbf33aca36a6e7464148f
Fagner Brack <github@fagnermartins.com>
2016-10-16 02:40:56 +1100

    Implement beforeEach/afterEach for the git push specs

--- test/unit/routines/git-push.spec.js
+++ test/unit/routines/git-push.spec.js
@@ -7,24 +7,35 @@ const gitCommit = require(root + "/src/routines/git-commit");
 const cloneToLocalDir = require(root + "/test/dummy-data/clone-repo-to-local-dir");
 
 describe("git-push", function() {
-  it("should push the repository sucessfully to the remote", function() {
-    let clonedTestRepository, dummyCommitId, defaultTestRepository;
-    return startGitRepoWithServer().then(function(defaultTestRepositoryResult) {
-      defaultTestRepository = defaultTestRepositoryResult;
-      return cloneToLocalDir(defaultTestRepository.gitHttpUrl);
-    }).then(function(clonedTestRepositoryResult) {
-      clonedTestRepository = clonedTestRepositoryResult;
-      return gitCommit("Dummy commit", clonedTestRepository.repository);
-    }).then(function(oid) {
-      dummyCommitId = oid;
-      return gitPush(defaultTestRepository.remotes[0].name, clonedTestRepository.repository);
-    }).then(function() {
-      return defaultTestRepository.remotes[0].repository.getCommit(dummyCommitId);
-    }).then(function(commit) {
-      expect(commit.message()).to.equal("Dummy commit");
-    }).finally(function() {
+  let defaultTestRepository;
+  let clonedTestRepository;
+
+  describe('Given a dummy server with git enabled cloned to local dir', function() {
+
+    beforeEach(function() {
+      return startGitRepoWithServer().then(function(defaultTestRepositoryResult) {
+        defaultTestRepository = defaultTestRepositoryResult;
+        return cloneToLocalDir(defaultTestRepository.gitHttpUrl);
+      }).then(function(clonedTestRepositoryResult) {
+        clonedTestRepository = clonedTestRepositoryResult;
+      });
+    });
+
+    afterEach(function() {
       defaultTestRepository.destroy();
       clonedTestRepository.remove();
+    });
+
+    it("should commit and push the repository sucessfully to the remote", function() {
+      let dummyCommitId;
+      return gitCommit("Dummy commit", clonedTestRepository.repository).then(function(oid) {
+        dummyCommitId = oid;
+        return gitPush(defaultTestRepository.remotes[0].name, clonedTestRepository.repository);
+      }).then(function() {
+        return defaultTestRepository.remotes[0].repository.getCommit(dummyCommitId);
+      }).then(function(commit) {
+        expect(commit.message()).to.equal("Dummy commit");
+      });
     });
   });
 });
