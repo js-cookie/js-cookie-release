44ef0665e9e8e5fdf7c6bfcd61f95fe8b6990a4b
Fagner Brack <github@fagnermartins.com>
2017-04-03 22:39:34 +1000

    Release itself

---
+++ README.md
@@ -0,0 +1,15 @@
+## Operations
+
+#### `Promise bumpJSONFiles(String bumpSpec, Array filePaths)`
+
+Bumps the "version" property from a list of file paths that matches the [JSON](http://json.org/) spec.  
+Returns a `Promise` that will resolve once all files have been bumped.
+
+#### `Promise gitCommit(String message, NodeGitRepository gitRepository)`
+
+Adds all the changes to the staging area and create a commit to the given repository.  
+Returns a `Promise` that will resolve once the commit has been created.
+
+## Release Steps
+
+* Run `npm run release <bumpSpec>`, where `bumpSpec` is either `patch`, `minor` or `major`
---
+++ release.js
@@ -0,0 +1,50 @@
+const Promise = require("bluebird");
+const Git = require("nodegit");
+
+const bumpJSONFiles = require("./src/file/bump-json-files");
+const gitCommit = require("./src/git/git-commit");
+
+let targetBumpSpec;
+if (process.argv.includes("patch")) {
+  targetBumpSpec = "patch";
+}
+if (process.argv.includes("minor")) {
+  targetBumpSpec = "minor";
+}
+if (process.argv.includes("major")) {
+  targetBumpSpec = "major";
+}
+if (!targetBumpSpec) {
+  console.log("Invalid bump spec, use 'patch', 'minor' or 'major'");
+  return;
+}
+
+const isFakeRun = process.argv.includes("fake");
+
+let localRepo;
+
+Promise.try(() => {
+  console.log("Bumping package.json...");
+  if (!isFakeRun) {
+    return bumpJSONFiles(targetBumpSpec, ["package.json"]);
+  }
+}).then(() => {
+  return Git.Repository.discover(".", 0, ".");
+}).then((_localRepo) => {
+  localRepo = _localRepo
+  console.log("Found repository:", localRepo);
+  console.log("Creating release commit...");
+  if (!isFakeRun) {
+    return gitCommit("Release new version", localRepo);
+  }
+}).then((commitObjectId) => {
+  const tagName = commitObjectId && commitObjectId.substring(0, 8)
+  const tagReferenceCommit = commitObjectId
+  console.log("Creating tag:", tagName);
+  if (!isFakeRun) {
+    return gitTag(tagName, tagReferenceCommit, localRepo);
+  }
+});
+
+// TODO Push the tag to the remote
+// TODO Publish on NPM
